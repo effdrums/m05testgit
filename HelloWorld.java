b217c57daea82392cdf9bfb41785e9a9863258f2
Efrem Blazquez  Aleman <eblazquez@proven.local>
2024-02-07 17:36:54 +0100

    closes #1; test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 
 static public void main(String args[]){
-	System.out.println("Hola M05");
+	System.out.println("Hola M05 Test");
 	System.out.println("La meva SUPER APP");
 
 }
